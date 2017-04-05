107da74acd732e7dfb4d7849d75d03f2f968d70c
dannyhodge <dannyhodge95@gmail.com>
2017-04-05 11:33:25 +0100

    Old stuff
    save me kiki d

---
+++ Assets/Scripts/TileMapMouse.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(tileMap))]
+public class TileMapMouse : MonoBehaviour {
+
+    tileMap _tileMap;
+    public GameObject House1;
+    public GameObject House2;
+    public GameObject House3;
+    bool select1;
+    bool select2;
+    bool select3;
+
+    Vector3 currentTileCoord;
+
+    public Transform selectionCube;
+    void Start()
+    {
+        _tileMap = GetComponent<tileMap>();
+    }
+	// Update is called once per frame
+	void Update () {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        if ( GetComponent<Collider>().Raycast(ray, out hitInfo, Mathf.Infinity))
+        {
+            int x = Mathf.FloorToInt(hitInfo.point.x / _tileMap.tileSize);
+            int z = Mathf.FloorToInt(hitInfo.point.z / _tileMap.tileSize);
+           // Debug.Log("Tile: " + x + "," + z);
+
+            currentTileCoord.x = x;
+            currentTileCoord.z = z;
+
+            selectionCube.transform.position = currentTileCoord;
+        }
+        else
+        {
+          
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Click!");
+            Debug.Log("Tile: " + currentTileCoord.x + "," + currentTileCoord.z);
+            if (select1)
+            {
+                Instantiate(House1, new Vector3(currentTileCoord.x + 0.5f, 0.0f, currentTileCoord.z + 0.5f), Quaternion.identity);
+            }
+            else if (select2)
+            {
+                Instantiate(House2, new Vector3(currentTileCoord.x + 0.5f, 0.0f, currentTileCoord.z + 0.5f), Quaternion.identity);
+            }
+            else if (select3)
+
+            {
+                Instantiate(House3, new Vector3(currentTileCoord.x + 0.5f, 0.0f, currentTileCoord.z + 0.5f), Quaternion.identity);
+            }
+
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            select1 = true;
+            select2 = false;
+            select3 = false;
+            Debug.Log("House 1 selected");
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            select1 = false;
+            select2 = true;
+            select3 = false;
+            Debug.Log("House 2 selected");
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            select1 = false;
+            select2 = false;
+            select3 = true;
+            Debug.Log("House 3 selected");
+
+        }
+    }
+}
---
+++ Assets/Scripts/camera.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+//	摄像机移动方向
+public class camera : MonoBehaviour 
+{
+	public float x = 0, y = 0;
+	public GameObject camera_control;	//	当前目标
+	public float move_speed,rotate_speed,scale_speed;	//	camera parameter
+	private int wide, heigh; //monitor size
+	private bool click_right_mouse = false;	
+
+
+
+	void initialization () 
+	{
+		wide = Screen.width;
+		heigh = Screen.height;
+	}
+
+	void Update () 
+	{
+		RotateCamera ();
+		ScaleCamera ();
+		if (!click_right_mouse) 
+		{
+			MoveCamera ();
+		}
+	}
+	private void RotateCamera () 
+	{
+
+	}
+		
+	private void ScaleCamera () 
+	{
+		float mouseScr = Input.GetAxis ("Mouse ScrollWheel");
+		if (mouseScr != 0f) 
+		{
+			mouseScr = mouseScr > 0 ? 1f : -1f;
+			transform.position += transform.forward * Time.deltaTime * scale_speed * mouseScr;
+		}
+	}
+	//	移动摄像机
+	private void MoveCamera () 
+	{  
+		if (Input.GetMouseButtonDown(0))
+		{
+		float position_x, position_y;
+		position_x = -Input.GetAxis("Mouse X")*move_speed;
+		position_y = -Input.GetAxis("Mouse Y")*move_speed;
+			transform.Translate(position_x,position_y,0);
+		}
+	}
+}
